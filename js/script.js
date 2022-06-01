7d4670ad66d1ee09a477ee9fca3e808d8ee3b5fd
Pamela <pamela.tab89@gmail.com>
2022-06-01 17:17:07 -0300

    condicao de game over

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
 
 
 const jump = () => {
@@ -10,5 +12,25 @@ const jump = () => {
 
 }
 
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        pipe.style.animation = 'none';
+        pipe.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+    
+}, 10);
 
 document.addEventListener('keydown', jump);
+console.log(jump)
